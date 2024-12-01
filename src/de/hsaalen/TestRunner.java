a22be77081e860b53ed0a101d1ff08132e71c925
Paddy <patrickkellner002@gmail.com>
2024-12-01 16:17:31 +0100

    renamed class Snake to SnakeGame added class Snake and enum Direction. class Snake is not yet used in Board (Aufgabe 2a, 3. Refactoring Kellner)

--- src/de/hsaalen/TestRunner.java
+++ src/de/hsaalen/TestRunner.java
@@ -7,12 +7,13 @@ import org.junit.runner.notification.Failure;
 public class TestRunner {
    public static void main(String[] args) 
    {
-      Result result = JUnitCore.runClasses(BoardTest.class);
-		
-      for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
+      Result result = JUnitCore.runClasses(BoardTest.class,SnakeTest.class);
+      int i = 0;
+      for ( Failure failure : result.getFailures() ) {
+         System.out.println( "Failure" + " " + i + " " + failure.getDescription() );
+         System.out.println( "  " + failure.getTrace() );
+
+         i++;
       }
-		
-      System.out.println(result.wasSuccessful());
    }
 }
